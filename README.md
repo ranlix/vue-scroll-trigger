0056d8e3b6b01f3ed259d608338f8b9329714e3a
liran319 <liran319@gmail.com>
2020-11-03 18:06:57 +0800

    init add plugin

--- README.md
+++ README.md
@@ -1,2 +1,43 @@
 # vue-scroll-trigger
-A Vue plugin which supervises binding element's scrolling and trigger target elements class 
+A Vue plugin which supervises binding element's scrolling and trigger target elements class
+
+# Install
+
+#### with `yarn` :
+`yarn add vue-scroll-trigger`
+
+#### or with `npm` :
+`npm install vue-scroll-trigger --save`
+
+# Usage
+
+```javascript
+import vueScrollTrigger from 'vue-scroll-trigger'
+
+Vue.use(vueScrollTrigger);
+```
+
+```html
+  <div class="menu">
+    <ul>
+      <li id="menu-item-1">menu item 1</li>
+      <li id="menu-item-1">menu item 2</li>
+      <li id="menu-item-1">menu item 3</li>
+      <li id="menu-item-1">menu item 4</li>
+    </ul>
+  </div>
+  <div class="container office__container">
+    <section id="profile" v-scroll-trigger="{ targetId: 'menu-item-1' }">
+      section block
+    </section>
+    <section id="people" v-scroll-trigger="{ targetId: 'menu-item-2' }">
+      section block
+    </section>
+    <section id="awards" v-scroll-trigger="{ targetId: 'menu-item-3' }">
+      section block
+    </section>
+    <section id="joinus" v-scroll-trigger="{ targetId: 'menu-item-4' }">
+      section block
+    </section>
+  </div>
+```
---
+++ src/lib/index.js
@@ -0,0 +1,51 @@
+const install = function(Vue) {
+  /**
+   * @function inViewport: Verify binding element whether in window viewport
+   */
+  Vue.directive('scroll-trigger', {
+    inViewport (el) {
+      var rect = el.getBoundingClientRect()
+      return !(rect.bottom < 0 || rect.top > window.innerHeight)
+    },
+
+    bind (el, binding) {
+      el.$onScroll = () => {
+        const { targetId } = binding.value
+        if (!targetId) {
+          binding.def.unbind(el, binding)
+          return
+        }
+        const targetElement = document.getElementById(targetId)
+        if (!targetElement) {
+          binding.def.unbind(el, binding)
+          return
+        }
+        if (binding.def.inViewport(el)) {
+          // if binding element is in viewport, toggle className of targetElement
+          targetElement.classList.add('active')
+        } else {
+          // if binding element is in viewport, remove className of targetElement
+          targetElement.classList.remove('active')
+        }
+      }
+      document.addEventListener('scroll', el.$onScroll)
+    },
+
+    inserted (el) {
+      el.$onScroll()
+    },
+
+    unbind (el) {
+      document.removeEventListener('scroll', el.$onScroll)
+      delete el.$onScroll
+    }
+  })
+}
+
+if (typeof window !== 'undefined' && window.Vue) {
+  install(window.Vue)
+}
+
+export default {
+  install
+}
